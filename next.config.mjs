5dc8a5de07b957599c865951394e21c3bf0f1fc2
Lebron Rumeral <lebronrumeral@gmail.com>
2025-10-08 22:13:10 +0800

    HUUHU

--- next.config.mjs
+++ next.config.mjs
@@ -19,19 +19,18 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' blob: data:;
-              font-src 'self';
-              object-src 'none';
-              base-uri 'self';
-              form-action 'self';
-              frame-ancestors 'none';
-              connect-src 'self' https://api.open-meteo.com https://openweathermap.org https://*.openweathermap.org https://bfxjidnfgrconxvdvjmh.supabase.co https://*.supabase.co wss://bfxjidnfgrconxvdvjmh.supabase.co;
-              upgrade-insecure-requests;
-            `.replace(/\s{2,}/g, ' ').trim()
+            value:
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'; " +
+              "style-src 'self' 'unsafe-inline'; " +
+              "img-src 'self' blob: data:; " +
+              "font-src 'self'; " +
+              "object-src 'none'; " +
+              "base-uri 'self'; " +
+              "form-action 'self'; " +
+              "frame-ancestors 'none'; " +
+              "connect-src 'self' https://api.open-meteo.com https://openweathermap.org https://*.openweathermap.org https://bfxjidnfgrconxvdvjmh.supabase.co https://*.supabase.co wss://bfxjidnfgrconxvdvjmh.supabase.co; " +
+              "upgrade-insecure-requests;"
           }
         ],
       },
