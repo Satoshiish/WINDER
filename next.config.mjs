d9993fe0d1db6be10ab37ca54dd3287246be2eb7
Lebron Rumeral <lebronrumeral@gmail.com>
2025-10-08 22:06:10 +0800

    CSP

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,27 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' blob: data:;
+              font-src 'self';
+              object-src 'none';
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'none';
+              upgrade-insecure-requests;
+              connect-src 'self' https://api.open-meteo.com https://openweathermap.org https://*.openweathermap.org https://bfxjidnfgrconxvdvjmh.supabase.co https://*.supabase.co wss://bfxjidnfgrconxvdvjmh.supabase.co;
+            `.replace(/\s{2,}/g, ' ').trim()
+          }
+        ],
+      },
+      {
         source: '/manifest.json',
         headers: [
           {
