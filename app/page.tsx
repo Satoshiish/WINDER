dc132ee9c9138443076828b310bab8d55f156851
Lebron Rumeral <lebronrumeral@gmail.com>
2025-10-19 13:58:47 +0800

    badge

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-
+import { Badge } from "@/components/ui/badge"
 import { Label } from "@/components/ui/label"
 import {
   AlertTriangle,
